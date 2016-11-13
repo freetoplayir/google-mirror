2c329ae24200569f2449e2e081fda21deb64739c
Siu <SiuChuenChih@outlook.com>
2016-11-13 19:30:05 +0800

    search.js 重构

--- routes/search.js
+++ routes/search.js
@@ -6,37 +6,33 @@ var REQ_URL = require('../google');
 
 
 /* GET users listing. */
-router.get('/',  function(req, res, next) {
-  var url;
-  if (req.query.q) {
+router.get('/', function (req, res, next) {
+    var url;
+    if (!req.query.q) {
+        res.render('index');
+        return;
+    }
     url = REQ_URL;
 
     for (var i in req.query) {
-      url = url + '&' + i + '=' + urlencode(req.query[i]);
+        url = url + '&' + i + '=' + urlencode(req.query[i]);
     }
     console.log(url);
 
     request(url, function (err, response, body) {
-      var para;
-
-      if (!err && response.statusCode == 200) {
-        para = JSON.parse(body);
-
-        if (para.error) {
-          res.render('index');
-        } else {
-          para.q = req.query.q;
-          res.render('search', para);
+        if (err && response.statusCode != 200) {
+            res.render('index');
+            return;
         }
-
-      } else {
-        res.render('index');
-      }
+        var para = JSON.parse(body);
+        if (para.error) {
+            res.render('index');
+            return;
+        }
+        para.q = req.query.q;
+        res.render('search', para);
     });
 
-  } else {
-    res.render('index');
-  }
 });
 
 module.exports = router;
